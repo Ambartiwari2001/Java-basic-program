d56ba24105dec5fa7b1a11b487a8d2e74d7b5fb0
Ambar Tiwari <127751134+Ambartiwari2001@users.noreply.github.com>
2023-09-15 22:26:04 +0530

    Add files via upload
    simple example to multiply two matrices of 3 rows and 3 columns.

---
+++ MatrixMulti.java
@@ -0,0 +1,35 @@
+//Java Program to multiply two matrices.
+
+ class MatrixMulti {
+
+
+		public static void main(String[] args) {
+			
+			int[][] m1= {{1,2,3},{4,3,2},{5,4,6}};
+	    	int[][] m2= {{3,1,2},{2,3,2},{7,3,6}};
+	    	int[][] m3=new int[3][3];
+	    	
+	    	for(int i=0;i<3;i++)
+	    	{
+	    		for(int j=0;j<3;j++)
+	    		
+	    		{
+	    		    for(int k=0;k<3;k++)
+	    		    {
+	    			m3[i][j]  =m3[i][j] +m1[i][k] * m2[k][j]; 
+	    		    }
+	    		}
+	    	
+	    	}
+	    	for(int i=0;i<3;i++)
+	    	{
+	    		for(int j=0;j<3;j++)
+	    		
+	    		{
+	    			System.out.print(m3[i][j]+" "); 
+	    		}
+	    	  System.out.println();
+}
+	
+}
+}
